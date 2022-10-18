580ee235ba9ecf9b5ebcfa156e6a936a399742f4
Jake Anderson <jake94a@gmail.com>
2022-10-18 14:29:48 -0600

    trying bug fix

--- tests/test_a_thing.py
+++ tests/test_a_thing.py
@@ -7,18 +7,24 @@ def postgres_conn(ip, port):
     user = "postgres"
     password = "test"
     try:
-        conn = psycopg2.connect(dbname=dbname, user=user, host=ip, port=port, password=password)
+        conn = psycopg2.connect(
+            dbname=dbname,
+            user=user,
+            host=ip,
+            port=port,
+            password=password,
+        )
         cursor = conn.cursor()
         cursor.execute("CREATE TABLE test (hello VARCHAR(24));")
         cursor.execute("INSERT INTO test (hello) VALUES (123);")
         cursor.execute("SELECT * FROM test;")
         records = cursor.fetchall()
-        print('records', records)
+        print("records", records)
         cursor.close()
         conn.close()
         return True
     except:
-        raise Exception("Connection failed")
+        return False
 
 
 @pytest.fixture(scope="session")
@@ -27,11 +33,9 @@ def db_service(docker_ip, docker_services):
 
     # `port_for` takes a container port and returns the corresponding host port
     port = docker_services.port_for("db", 5432)
-    conn = postgres_conn(docker_ip, port)
     docker_services.wait_until_responsive(
         timeout=60.0, pause=0.1, check=lambda: postgres_conn(docker_ip, port)
     )
-    print('conn', conn)
     return True
 
 
